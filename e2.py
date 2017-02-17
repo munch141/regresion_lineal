164b05a465f950c6f07eb36e31cfb3097ef30277
ricardo <munch_141@hotmail.com>
2017-02-17 14:08:06 -0400

    el modelo está listo...puse las gráficas del primer ejercicio ahí mas o menos..faltan los otros 2

---
+++ e2.py
@@ -0,0 +1,74 @@
+import modelo_lineal
+
+
+def leer_archivo(filename):
+    f = open(filename, 'r')
+
+    while (True):
+        line = f.readline()
+        if line[0] != "#":
+            break
+        else:
+            continue
+
+    columnas = int(line.split()[0])
+
+    line = f.readline()
+    filas = int(line.split()[0])
+
+    line = f.readline()
+    rasgos = []
+    for i in range(columnas-1):
+        line = f.readline()
+        rasgos.append(line)
+
+    x = []
+    y = []
+    for i in range(filas):
+        line = f.readline()
+        split = line.split()
+        x.append([])
+        for j in range(columnas-2):
+            x[i].append(float(split[j+1]))
+        y.append(float(split[columnas-1]))
+
+    return modelo_lineal.Modelo(x, y, rasgos)
+
+
+###############################################################################
+# DATOS SOBRE PESO (1.1)
+###############################################################################
+modelo = leer_archivo("data/x01.txt")
+modelo.normalizar()
+costos = modelo.gradient_descent(0.1)
+#modelo.imprimir()
+
+import matplotlib.pyplot as plt
+
+# ejemplos
+x = modelo.x
+y = modelo.y
+
+# curva de prediccion
+x2 = [i-1 for i in range(10)]
+h = map(lambda e: modelo.hipotesis(e), x2)
+
+# costos por iteracion de descenso del gradiente
+iteraciones = [i for i in range(len(costos))]
+
+#plt.plot(x, y, 'ro')
+#plt.plot(x2, y2)
+#plt.plot(iteraciones, costos)
+#plt.show()
+
+###############################################################################
+# DATOS SOBRE MORTALIDAD (1.2)
+###############################################################################
+modelo = leer_archivo("data/x08.txt")
+modelo.normalizar()
+costos = modelo.gradient_descent(0.1)
+modelo.imprimir()
+
+iteraciones = [i for i in range(len(costos))]
+#plt.plot(iteraciones, costos)
+#plt.show()
---
+++ modelo_lineal.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+class Modelo():
+    def __init__(self, x, y, rasgos, theta=[]):
+        self.x = x
+        self.y = y
+        self.rasgos = rasgos
+
+        if not theta:
+            self.theta = [1.0 for i in range(len(x[0])+1)]
+        else:
+            self.theta = theta
+
+    def imprimir(self):
+        print "rasgos: ", self.rasgos
+        print "theta: ", self.theta
+        print
+        print "ejemplos:"
+        for i in range(len(self.x)):
+            print (self.x[i], self.y[i])
+
+    def normalizar(self):
+        medias = np.mean(self.x, 0)  # medias de las columnas
+        desv = np.std(self.x, 0)     # desviaciones estandar de las columnas
+
+        self.x = ((self.x - medias) / desv).tolist()
+
+    def hipotesis(self, x):
+        return self.theta[0]+np.dot(self.theta[1:], x)
+
+    def derivada(self, j):
+        r = 0.0
+
+        if j == 0:
+            for i in range(len(self.x)):
+                r += (self.hipotesis(self.x[i])-self.y[i])
+        else:
+            for i in range(len(self.x)):
+                r += (self.hipotesis(self.x[i])-self.y[i])*self.x[i][j-1]
+
+        return r/len(self.x)
+
+    def gradient_descent(self, alpha=0.01, max_it=1000):
+        temp = [0.0 for i in range(len(self.theta))]
+        J = []
+
+        while (i < max_it):
+            for j in range(len(self.theta)):
+                temp[j] = self.theta[j] - alpha*self.derivada(j)
+            self.theta = temp[:]
+            J.append(self.costo())
+            i += 1
+        return J
+
+    def costo(self):
+        r = 0.0
+        for i in range(len(self.x)):
+            r += (self.hipotesis(self.x[i])-self.y[i])**2
+
+        return r/(2*len(self.x))
